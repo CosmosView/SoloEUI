b95fcebcbcd81fea3eb3089e7985ab060c40186e
CosmosView <zzd8885@gmail.com>
2024-06-02 10:32:52 +0800

    ver4.1

--- createhtml.py
+++ createhtml.py
@@ -35,12 +35,18 @@ def generate_html(images_folder):
             height: auto; /* 图片高度自动调整 */
             margin: 0 10px; /* 图片之间和容器边缘的间距 */
         }}
+        p:last-of-type {{
+            width: 60%;
+            margin: 0 auto; /* 上下无间距，左右自动调整以居中 */
+            text-align: left;
+        }}
     </style>
 </head>
 <body>
     <h1>Latest Images of Solar Orbiter's Extreme Ultraviolet Imager (EUI)</h1>
     <p>Generated at {current_utc_time}</p>
-    <a href="https://www.sidc.be/EUI/data-analysis">Data Source</a>
+    <a href="https://www.sidc.be/EUI/data-analysis" target="_blank">Data Source</a> &nbsp; &nbsp; <a href="https://github.com/CosmosView/SoloEUI" target="_blank">Project Home</a>
+    
     <div class="gallery">
 '''
 
@@ -55,10 +61,17 @@ def generate_html(images_folder):
             html_content += f'<img src="{html_image_path}/{image_name}" alt="{image_name}">'
             count += 1
 
-    if count % 2 != 0:
-        html_content += '</div>'
 
-    html_content += '</div></body></html>'
+    html_content += '</div>'
+
+    html_content += '''
+    </div>
+    <br>
+    <p><b>Note:</b>The 0° latitude and 0°/180° longitude lines in the Stonyhurst heliographic coordinates were plotted on the images to help determine the positional relationship between the satellite and the Earth. Details of the satellite's location can be found <a href="https://www.cosmos.esa.int/web/solar-orbiter/where-is-solar-orbiter" target="_blank">here</a>. <b>Data are uncalibrated and should be used with caution.</b> </p>
+    <br><br>
+</body>
+</html>
+    '''
 
     with open(html_save_path + 'index.html', 'w') as file:
         file.write(html_content)
